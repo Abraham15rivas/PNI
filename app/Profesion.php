b9a387a31867f0b6ed9e4b1d0a67f8efb61dddb3
Abraham15rivas <abraham15rivas@gmail.com>
2020-08-18 23:12:30 -0400

    agregado

---
+++ app/Profesion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profesion extends Model
+{
+    protected $table = 'profesiones';
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::prefix('statistics')->group(function () {
+    Route::get('/investigators','InvestigatorController@index');
+});
+
+Route::get('', function () {
     return view('layouts.app');
 });
+
+Route::get('home', function () {
+    return view('layouts.app');
+});
+
+Route::get('researcher', function () {
+    return view('layouts.app');
+});
+
+Route::get('research', function () {
+    return view('layouts.app');
+});
+

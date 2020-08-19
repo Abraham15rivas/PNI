91de4a3dd04dfb91f0ce6c4609fdc776d42e8473
Abraham15rivas <abraham15rivas@gmail.com>
2020-08-19 15:47:48 -0400

    estadistica rango de edades

--- app/Http/Controllers/InvestigatorController.php
+++ app/Http/Controllers/InvestigatorController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Investigator;
 use App\Profesion;
 use App\State;
+use Carbon\Carbon;
 
 class InvestigatorController extends Controller
 {
@@ -56,14 +57,68 @@ class InvestigatorController extends Controller
             $groupState->push(["estado"=>$name,"total"=>$total]);
         }
         
+        // Investigadores por rango de edad
+        $birthDate =  $investigators;
+        $groupRangeAge = self::rangeAge($birthDate);
+      
+        // Edad minima, maxima y promedio
+        $groupAge = collect();
+
         $data = collect([
             "total_investigators"=>$total_investigators,
             "investigators_mens"=>$investigators_mens,
             "investigators_womens"=>$investigators_womens,
             "groupProfesion"=>$groupProfesion,
-            "groupStates"=>$groupState
+            "groupStates"=>$groupState,
+            "groupRangeAge"=>$groupRangeAge,
+            "groupAge"=>$groupAge
         ]);
 
         return $data->toJson();
+    }
+
+    public function rangeAge ($birthDate) {
+
+        $ages = collect();
+        
+        foreach ($birthDate as $date) {
+            $ages->push(Carbon::parse($date->fecha_nac)->age);
+        }
+
+        $rangos = collect([
+            "00 - 09" => 0,
+            "10 - 19" => 0,
+            "20 - 29" => 0,
+            "30 - 39" => 0,
+            "40 - 49" => 0,
+            "50 - 59" => 0,
+            "60 - 69" => 0,
+            "70 - 79" => 0,
+            "80 - 89" => 0,
+        ]);
+
+        foreach ($ages as $age) 
+        {
+            if (0 <= $age && $age <= 9) {
+                $rangos["00 - 09"] += 1;
+            } else if (10 <= $age && $age <= 19) {
+                $rangos["10 - 19"] += 1;
+            } else if (20 <= $age && $age <= 29) {
+                $rangos["20 - 29"] += 1;
+            } else if (30 <= $age && $age <= 39) {
+                $rangos["30 - 39"] += 1;
+            } else if (40 <= $age && $age <= 49) {
+                $rangos["40 - 49"] += 1;
+            } else if (50 <= $age && $age <= 59) {
+                $rangos["50 - 59"] += 1;
+            } else if (60 <= $age && $age <= 69) {
+                $rangos["60 - 69"] += 1;
+            } else if (70 <= $age && $age <= 79) {
+                $rangos["70 - 79"] += 1;
+            } else if (80 <= $age && $age <= 89) {
+                $rangos["80 - 89"] += 1;
+            } 
+        }
+        return $rangos;
     }
 }

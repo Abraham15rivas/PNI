72f28ad4294beab03d261566e81d33ca7ed3f74e
Abraham15rivas <abraham15rivas@gmail.com>
2020-08-19 23:19:20 -0400

    Intereses de investigación

--- app/Http/Controllers/InvestigatorController.php
+++ app/Http/Controllers/InvestigatorController.php
@@ -11,71 +11,88 @@ use Carbon\Carbon;
 class InvestigatorController extends Controller
 {
     public function index(){
-        //obtener datos masivos
-        $investigators = Investigator::get();
-        $profesion = Profesion::get();
-        $states = State::get();
+        if (!isset($_GET['interest'])) {
+            //obtener datos masivos
+            $investigators = Investigator::get();
+            $profesion = Profesion::get();
+            $states = State::get();
 
-        //Numero total de investigadores
-        $total_investigators = $investigators->count();
+            //Numero total de investigadores
+            $total_investigators = $investigators->count();
 
-        //Numero de investigadores separados por genero
-        $investigators_mens = $investigators->where('id_genero',2)->count();
-        $investigators_womens = $investigators->where('id_genero',1)->count();
+            //Numero de investigadores separados por genero
+            $investigators_mens = $investigators->where('id_genero',2)->count();
+            $investigators_womens = $investigators->where('id_genero',1)->count();
 
-        //Numero de investigadores agrupados por carreras
-        $groupByPro = $investigators->groupBy('id_profesion');
+            //Numero de investigadores agrupados por carreras
+            $groupByPro = $investigators->groupBy('id_profesion');
 
-        $groupProfesion = collect();
+            $groupProfesion = collect();
 
-        foreach($groupByPro as $key => $val){
-            $selected = $profesion->where('id_profesion',$key);
+            foreach($groupByPro as $key => $val){
+                $selected = $profesion->where('id_profesion',$key);
 
-            //destructurar el objeto para obtener el nombre de la profesion
-            foreach($selected as $pro){
-                $name = $pro->profesion;
+                //destructurar el objeto para obtener el nombre de la profesion
+                foreach($selected as $pro){
+                    $name = $pro->profesion;
+                }
+
+                $total = count($val);
+                $groupProfesion->push(["profesion"=>$name,"total"=>$total]);
             }
 
-            $total = count($val);
-            $groupProfesion->push(["profesion"=>$name,"total"=>$total]);
-        }
+            //Numero de investigadores por estados
+            $groupBySta = $investigators->groupBy('id_estado');
 
-        //Numero de investigadores por estados
-        $groupBySta = $investigators->groupBy('id_estado');
+            $groupState = collect();
 
-        $groupState = collect();
+            foreach($groupBySta as $key => $val){
+                $selected = $states->where('id_estado',$key);
 
-        foreach($groupBySta as $key => $val){
-            $selected = $states->where('id_estado',$key);
+                //destructurar el objeto para obtener el nombre del estado
+                foreach($selected as $pro){
+                    $name = $pro->estado;
+                }
 
-            //destructurar el objeto para obtener el nombre del estado
-            foreach($selected as $pro){
-                $name = $pro->estado;
+                $total = count($val);
+                $groupState->push(["estado"=>$name,"total"=>$total]);
             }
+            
+            // Investigadores por rango de edad, edad minima, maxima y promedios por genero
+            $calculations = self::rangeAge($investigators);
+            $groupRangeAge = $calculations["ranges"];
+            $groupAverageAge = $calculations["averages"];
 
-            $total = count($val);
-            $groupState->push(["estado"=>$name,"total"=>$total]);
+            // Numero de instituciones
+            $groupInstitution = self::institutionType($investigators);
+
+            $data = collect([
+                "total_investigators"=>$total_investigators,
+                "investigators_mens"=>$investigators_mens,
+                "investigators_womens"=>$investigators_womens,
+                "groupProfesion"=>$groupProfesion,
+                "groupStates"=>$groupState,
+                "groupRangeAge"=>$groupRangeAge,
+                "groupAverageAge"=>$groupAverageAge,
+                "groupInstitution"=>$groupInstitution
+            ]);
+        } else {
+            if($_GET['interest'] == 'true') {
+                //obtener datos masivos
+                $investigators = Investigator::get();
+
+                //Numero total de investigadores
+                $total_investigators = $investigators->count();
+
+                // Intereses de los investigadores
+                $groupInterest = collect();
+
+                $data = collect([
+                    "total_investigators"=>$total_investigators,
+                    "groupInterest"=>$groupInterest
+                ]);
+            }
         }
-        
-        // Investigadores por rango de edad, edad minima, maxima y promedios por genero
-        $calculations = self::rangeAge($investigators);
-        $groupRangeAge = $calculations["ranges"];
-        $groupAverageAge = $calculations["averages"];
-
-        // Numero de instituciones
-        $groupInstitution = self::institutionType($investigators);
-
-        $data = collect([
-            "total_investigators"=>$total_investigators,
-            "investigators_mens"=>$investigators_mens,
-            "investigators_womens"=>$investigators_womens,
-            "groupProfesion"=>$groupProfesion,
-            "groupStates"=>$groupState,
-            "groupRangeAge"=>$groupRangeAge,
-            "groupAverageAge"=>$groupAverageAge,
-            "groupInstitution"=>$groupInstitution
-        ]);
-
         return $data->toJson();
     }
 

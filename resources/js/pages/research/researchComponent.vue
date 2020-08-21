e62d2680319c036968f88db17b4d8fd554677ce4
Oricab <oriollacaballero10@gmail.com>
2020-08-20 19:36:19 -0430

    maqueta de interés de investigación

--- resources/js/pages/research/researchComponent.vue
+++ resources/js/pages/research/researchComponent.vue
@@ -1,7 +1,50 @@
 <template>
     <div class="container">
-        Vista de Investigaciones
+        <div class="row">
+            <div class="col s12">
+                <h5 class="center-align" >Indicador del tipo de institución e interés de investigación de las investigadoras e investigadores</h5>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center" >Tipo de Institución</span>
+                        Aquí
+                    </div>
+                </div>
+            </div>
+            <div class="col s6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center" >Interés de Investigación</span>
+                        Aquí 2
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center" >Interés de Investigación por Género</span>
+                        Aquí 3
+                    </div>
+                </div>
+            </div>
+            <div class="col s6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center" >Investigación Actual</span>
+                        Aquí 4
+                    </div>
+                </div>
+            </div>
+        </div>
+    
     </div>
+    
 </template>
 
 <script>

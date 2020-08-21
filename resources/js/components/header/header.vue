574cc63539abd09b143e41e5703a95e034db88dc
Oricab <oriollacaballero10@gmail.com>
2020-08-21 19:12:06 -0430

    cambios2

--- resources/js/components/header/header.vue
+++ resources/js/components/header/header.vue
@@ -1,45 +1,27 @@
 <template>
 <div>
     <header>
-<<<<<<< HEAD
-        <div >
-            <nav class="navbar" style="">
-                <div >
-                   <img src="images/cintillo.jpg" class="cintillo-min">
-=======
         <div class="navbar-fixed">
             <nav class="navbar blue darken-1"  style="height: 95px">
                 <div class="nav-wrapper">
                     <div class="brand-logo" >
                         <img class="logo-min header-logo" src="images/mincyt.png" alt="">
                     </div>
->>>>>>> cbce73ddbe5801961e3b258e14e6a381520683ee
                 </div>
             </nav>
         </div>
         <ul class=" sidenav sidenav-fixed">
             <li>
-<<<<<<< HEAD
-                <img src="images/pni.png" class="logo-min">
-=======
                 <div class="logo-container">
                     <img src="images/pni.png" class="logo-min">
                 </div>
->>>>>>> cbce73ddbe5801961e3b258e14e6a381520683ee
             </li>
             <li>
                 <ul class="collapsible collapsible-accordion">
-<<<<<<< HEAD
-                    <li>
-                        <h5 class="center">
-                            Indicadores
-                        </h5>
-=======
                     <li class="waves-effect">
                         <a class="collapsible-header" tabindex="0">
                            <h5 class="black-text name center">Indicadores</h5>
                         </a>
->>>>>>> cbce73ddbe5801961e3b258e14e6a381520683ee
                     </li>
                     <li class="waves-effect">
                         <router-link to="/researcher" class="collapsible-header" tabindex="0">
@@ -47,13 +29,8 @@
                         </router-link>
                     </li>
                     <li class="waves-effect">
-<<<<<<< HEAD
-                        <router-link to="/research" class="collapsible-header" tabindex="0">
-                            Interés de Investigaciones<i class="material-icons">list</i>
-=======
                         <router-link to="/researcher" class="collapsible-header" tabindex="0">
                             Investigadores e Investigadoras<i class="material-icons ">people</i> 
->>>>>>> cbce73ddbe5801961e3b258e14e6a381520683ee
                         </router-link>
                     </li>
                     <li class="waves-effect">
@@ -85,13 +62,8 @@
 
 .sidenav .logo-container {
     padding: 0 16px;
-<<<<<<< HEAD
-    height: 64px;
-    line-height: 0px;
-=======
     height: 94px;
     line-height: 64px;
->>>>>>> cbce73ddbe5801961e3b258e14e6a381520683ee
     font-size: 24px;
     box-sizing: content-box;
 }
@@ -103,28 +75,10 @@
 .sidenav .collapsible li, .sidenav.fixed .collapsible li {
     padding: 0;
 }
-<<<<<<< HEAD
-
-/* .chevron {
-    float: right;
-    height: 24px;
-    width: 24px;
-    line-height: 24px;
-    margin: 20px 0 0 0;
-    transition: transform .2s;
-    transform: rotate(-90deg);
-} */
-
-.logo-min {
-    width: 188px;
-    height: 100px;
-    margin: 5px 40px;
-=======
 .logo-min{
 	width: 195px;
 	height: 85px;
 	margin-top: 6px;
->>>>>>> cbce73ddbe5801961e3b258e14e6a381520683ee
 }
 .cintillo-min {
     width: 100%;

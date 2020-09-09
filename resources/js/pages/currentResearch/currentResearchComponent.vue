edd2d9b4ee8f93f68679973ad4359f7ed303fbea
Oricab <oriollacaballero10@gmail.com>
2020-09-09 17:28:51 -0430

    rama current con el dieño listo

--- resources/js/pages/currentResearch/currentResearchComponent.vue
+++ resources/js/pages/currentResearch/currentResearchComponent.vue
@@ -1,5 +1,75 @@
 <template>
     <div class="margin-x">
+        <div class="row">
+            <div class="col s12">
+                <h5 class="center-align" >Indicador del Módulo de la Investigación Actual</h5>
+            </div>
+        </div>
+        
+            <div class="row">
+                <div class="col s12 m4 offset-m4">
+                    <div class="card horizontal">
+                        <div class="card-image">
+                            <img class="icon-total-register" src="images/registro.svg">
+                        </div>
+                        <div class="card-stacked">
+                            <div class="card-content">
+                                <h6 class="center-align">Total registrados en el módulo de la investigación</h6>
+                            </div>
+                            <div class="card-action">
+                                    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center">Tipo de Institución</span>
+                        Aquí
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center" >Tipo de Investigación</span>
+                        Aquí
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title center">Línea de Investigación</span>
+                    Aquí
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center">Fase de la Investigación</span>
+                        Aquí
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title center" >Tiempo de la Investigación</span>
+                        Aquí
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,4 +82,5 @@
 .margin-x{
     margin: 0 50px
 }
+
 </style>

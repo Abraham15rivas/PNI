bf6776022bafe92978d144423f64433d655271fc
Rivas1526 <rivas15jimenez@gmail.com>
2021-01-07 23:47:44 -0400

    cambios menores

--- bootstrap/cache/config.php
+++ bootstrap/cache/config.php
@@ -10,7 +10,7 @@
     'locale' => 'en',
     'fallback_locale' => 'en',
     'faker_locale' => 'en_US',
-    'key' => 'base64:NXXjwtfNUftXwYHFzkdTyKr31lKBB+sS+36gcaNyYUo=',
+    'key' => 'base64:eDMJ8tDnCTPTkLw4JITpzhg3m5FK27jouk/IxLTo1Hc=',
     'cipher' => 'AES-256-CBC',
     'providers' => 
     array (
@@ -177,7 +177,7 @@
       'file' => 
       array (
         'driver' => 'file',
-        'path' => '/var/www/html/PNI/storage/framework/cache/data',
+        'path' => 'C:\\laragon\\www\\PNI\\storage\\framework/cache/data',
       ),
       'memcached' => 
       array (
@@ -340,10 +340,10 @@
     'orientation' => 'portrait',
     'defines' => 
     array (
-      'font_dir' => '/var/www/html/PNI/storage/fonts/',
-      'font_cache' => '/var/www/html/PNI/storage/fonts/',
-      'temp_dir' => '/tmp',
-      'chroot' => '/var/www/html/PNI',
+      'font_dir' => 'C:\\laragon\\www\\PNI\\storage\\fonts/',
+      'font_cache' => 'C:\\laragon\\www\\PNI\\storage\\fonts/',
+      'temp_dir' => 'C:\\Users\\Familia\\AppData\\Local\\Temp',
+      'chroot' => 'C:\\laragon\\www\\PNI',
       'enable_font_subsetting' => false,
       'pdf_backend' => 'CPDF',
       'default_media_type' => 'screen',
@@ -366,12 +366,12 @@
       'local' => 
       array (
         'driver' => 'local',
-        'root' => '/var/www/html/PNI/storage/app',
+        'root' => 'C:\\laragon\\www\\PNI\\storage\\app',
       ),
       'public' => 
       array (
         'driver' => 'local',
-        'root' => '/var/www/html/PNI/storage/app/public',
+        'root' => 'C:\\laragon\\www\\PNI\\storage\\app/public',
         'url' => 'http://localhost/storage',
         'visibility' => 'public',
       ),
@@ -388,7 +388,7 @@
     ),
     'links' => 
     array (
-      '/var/www/html/PNI/public/storage' => '/var/www/html/PNI/storage/app/public',
+      'C:\\laragon\\www\\PNI\\public\\storage' => 'C:\\laragon\\www\\PNI\\storage\\app/public',
     ),
   ),
   'hashing' => 
@@ -422,13 +422,13 @@
       'single' => 
       array (
         'driver' => 'single',
-        'path' => '/var/www/html/PNI/storage/logs/laravel.log',
+        'path' => 'C:\\laragon\\www\\PNI\\storage\\logs/laravel.log',
         'level' => 'debug',
       ),
       'daily' => 
       array (
         'driver' => 'daily',
-        'path' => '/var/www/html/PNI/storage/logs/laravel.log',
+        'path' => 'C:\\laragon\\www\\PNI\\storage\\logs/laravel.log',
         'level' => 'debug',
         'days' => 14,
       ),
@@ -478,7 +478,7 @@
       ),
       'emergency' => 
       array (
-        'path' => '/var/www/html/PNI/storage/logs/laravel.log',
+        'path' => 'C:\\laragon\\www\\PNI\\storage\\logs/laravel.log',
       ),
     ),
   ),
@@ -490,11 +490,11 @@
       'smtp' => 
       array (
         'transport' => 'smtp',
-        'host' => 'smtp.mailgun.org',
-        'port' => 587,
+        'host' => '',
+        'port' => '',
         'encryption' => 'tls',
-        'username' => NULL,
-        'password' => NULL,
+        'username' => '',
+        'password' => '',
         'timeout' => NULL,
         'auth_mode' => NULL,
       ),
@@ -535,7 +535,7 @@
       'theme' => 'default',
       'paths' => 
       array (
-        0 => '/var/www/html/PNI/resources/views/vendor/mail',
+        0 => 'C:\\laragon\\www\\PNI\\resources\\views/vendor/mail',
       ),
     ),
   ),
@@ -614,7 +614,7 @@
     'lifetime' => 120,
     'expire_on_close' => false,
     'encrypt' => false,
-    'files' => '/var/www/html/PNI/storage/framework/sessions',
+    'files' => 'C:\\laragon\\www\\PNI\\storage\\framework/sessions',
     'connection' => NULL,
     'table' => 'sessions',
     'store' => NULL,
@@ -634,9 +634,9 @@
   array (
     'paths' => 
     array (
-      0 => '/var/www/html/PNI/resources/views',
+      0 => 'C:\\laragon\\www\\PNI\\resources\\views',
     ),
-    'compiled' => '/var/www/html/PNI/storage/framework/views',
+    'compiled' => 'C:\\laragon\\www\\PNI\\storage\\framework\\views',
   ),
   'flare' => 
   array (
--- public/js/app.js
+++ public/js/app.js
@@ -24193,7 +24193,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.sidenav .logo-container[data-v-62e37494] {\n  padding: 0 16px;\n  height: 94px;\n  line-height: 64px;\n  font-size: 24px;\n  box-sizing: content-box;\n}\n.sidenav .collapsible > li.waves-effect[data-v-62e37494] {\n  display: block;\n}\n.sidenav .collapsible li[data-v-62e37494],\n.sidenav.fixed .collapsible li[data-v-62e37494] {\n  padding: 0;\n}\n.logo-min[data-v-62e37494] {\n  width: 195px;\n  height: 85px;\n  margin-top: 6px;\n}\n.cintillo-min[data-v-62e37494] {\n  width: 100%;\n  height: 100px;\n}\n.header-logo[data-v-62e37494] {\n  position: fixed;\n  right: 10px;\n}\n.brand-logo[data-v-62e37494] {\n  width: 100%;\n}\n.logo-sidebar[data-v-62e37494] {\n  height: 110px;\n  margin: 0 auto;\n  display: block;\n}\n.top-m[data-v-62e37494] {\n  margin-top: 10px;\n}\n@media (max-width: 992px) {\n.sidenav-collapse[data-v-62e37494] {\n    transform: translateX(0%);\n    transition: transform 300ms linear;\n}\n.button-collapse[data-v-62e37494] {\n    margin-left: 312px;\n    transition: margin-left 300ms linear;\n}\n}\n", ""]);
+exports.push([module.i, "\n.sidenav .logo-container[data-v-62e37494] {\r\n  padding: 0 16px;\r\n  height: 94px;\r\n  line-height: 64px;\r\n  font-size: 24px;\r\n  box-sizing: content-box;\n}\n.sidenav .collapsible > li.waves-effect[data-v-62e37494] {\r\n  display: block;\n}\n.sidenav .collapsible li[data-v-62e37494],\r\n.sidenav.fixed .collapsible li[data-v-62e37494] {\r\n  padding: 0;\n}\n.logo-min[data-v-62e37494] {\r\n  width: 195px;\r\n  height: 85px;\r\n  margin-top: 6px;\n}\n.cintillo-min[data-v-62e37494] {\r\n  width: 100%;\r\n  height: 100px;\n}\n.header-logo[data-v-62e37494] {\r\n  position: fixed;\r\n  right: 10px;\n}\n.brand-logo[data-v-62e37494] {\r\n  width: 100%;\n}\n.logo-sidebar[data-v-62e37494] {\r\n  height: 110px;\r\n  margin: 0 auto;\r\n  display: block;\n}\n.top-m[data-v-62e37494] {\r\n  margin-top: 10px;\n}\n@media (max-width: 992px) {\n.sidenav-collapse[data-v-62e37494] {\r\n    transform: translateX(0%);\r\n    transition: transform 300ms linear;\n}\n.button-collapse[data-v-62e37494] {\r\n    margin-left: 312px;\r\n    transition: margin-left 300ms linear;\n}\n}\r\n", ""]);
 
 // exports
 
@@ -24212,7 +24212,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.margin-x[data-v-54e7490d]{\n    margin: 0 50px;\n    padding-top: 32px;\n}\n.total-register[data-v-54e7490d]{\n\tfont-size: 40px;\n    margin-top: -8px;\n    margin-bottom: 0px;\n    text-align: center;\n}\n.icon-total-register[data-v-54e7490d]{\n\twidth: 110px;\n\theight: 110px;\n}\n.card-icon[data-v-54e7490d] {\n    background: #e3f2fd;\n    display: flex;\n    align-items: center;\n    padding: 0px 8px 0px 16px;\n}\n\n", ""]);
+exports.push([module.i, "\n.margin-x[data-v-54e7490d]{\r\n    margin: 0 50px;\r\n    padding-top: 32px;\n}\n.total-register[data-v-54e7490d]{\r\n\tfont-size: 40px;\r\n    margin-top: -8px;\r\n    margin-bottom: 0px;\r\n    text-align: center;\n}\n.icon-total-register[data-v-54e7490d]{\r\n\twidth: 110px;\r\n\theight: 110px;\n}\n.card-icon[data-v-54e7490d] {\r\n    background: #e3f2fd;\r\n    display: flex;\r\n    align-items: center;\r\n    padding: 0px 8px 0px 16px;\n}\r\n\r\n", ""]);
 
 // exports
 
@@ -24231,7 +24231,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.mt[data-v-b4ab9052]{\n    margin-top: 70px;\n    margin-bottom: 100px;\n}\n.padding[data-v-b4ab9052]{\n    padding-left: 50px;\n    padding-right: 50px;\n    font-family: \"Nunito\", Times, serif;\n}\n.imagen-portada[data-v-b4ab9052]{ width: 400px;\n}\n.section-imagen[data-v-b4ab9052]{\n    display: flex;\n    align-items: center;\n    height: 40vh;\n}\n", ""]);
+exports.push([module.i, "\n.mt[data-v-b4ab9052]{\r\n    margin-top: 70px;\r\n    margin-bottom: 100px;\n}\n.padding[data-v-b4ab9052]{\r\n    padding-left: 50px;\r\n    padding-right: 50px;\r\n    font-family: \"Nunito\", Times, serif;\n}\n.imagen-portada[data-v-b4ab9052]{ width: 400px;\n}\n.section-imagen[data-v-b4ab9052]{\r\n    display: flex;\r\n    align-items: center;\r\n    height: 40vh;\n}\r\n", ""]);
 
 // exports
 
@@ -24250,7 +24250,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.total-register[data-v-768de6a6]{\n\tfont-size: 40px;\n    margin-top: -8px;\n    margin-bottom: 0px;\n    text-align: center;\n}\n.icon-total-register[data-v-768de6a6]{\n\twidth: 110px;\n\theight: 110px;\n}\n.card-icon[data-v-768de6a6] {\n    background: #e3f2fd;\n    display: flex;\n    align-items: center;\n    padding: 0px 8px 0px 16px;\n}\n.title-card[data-v-768de6a6] {\n    text-align: center;\n    margin-top: 0px;\n}\n.separate[data-v-768de6a6] {\n    margin: 48px 64px;\n}\n.margin-x[data-v-768de6a6]{\n    margin: 0 50px;\n    padding-top: 32px;\n}\n.card[data-v-768de6a6]:hover {\n    background: #e3f2fd;\n}\n", ""]);
+exports.push([module.i, "\n.total-register[data-v-768de6a6]{\r\n\tfont-size: 40px;\r\n    margin-top: -8px;\r\n    margin-bottom: 0px;\r\n    text-align: center;\n}\n.icon-total-register[data-v-768de6a6]{\r\n\twidth: 110px;\r\n\theight: 110px;\n}\n.card-icon[data-v-768de6a6] {\r\n    background: #e3f2fd;\r\n    display: flex;\r\n    align-items: center;\r\n    padding: 0px 8px 0px 16px;\n}\n.title-card[data-v-768de6a6] {\r\n    text-align: center;\r\n    margin-top: 0px;\n}\n.separate[data-v-768de6a6] {\r\n    margin: 48px 64px;\n}\n.margin-x[data-v-768de6a6]{\r\n    margin: 0 50px;\r\n    padding-top: 32px;\n}\n.card[data-v-768de6a6]:hover {\r\n    background: #e3f2fd;\n}\r\n", ""]);
 
 // exports
 
@@ -24269,7 +24269,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.margin-x{\n    margin: 0 50px;\n    padding-top: 32px;\n}\n\n", ""]);
+exports.push([module.i, "\n.margin-x{\r\n    margin: 0 50px;\r\n    padding-top: 32px;\n}\r\n\r\n", ""]);
 
 // exports
 
@@ -24288,7 +24288,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\nhr[data-v-64b339f7]{ width: 100%\n}\n.md-button[data-v-64b339f7], .md-button-clean[data-v-64b339f7]{\n    background-color: white;\n}\n.card-icon[data-v-64b339f7] {\n    background: #e3f2fd;\n    display: flex;\n    align-items: center;\n    padding: 0px 8px 0px 16px;\n}\n.margin-x[data-v-64b339f7]{\n    margin: 0 25px;\n    padding-top: 32px;\n}\n.size-img[data-v-64b339f7]{ \n    width: 100px; \n    height: 100px;\n}\n.color-w[data-v-64b339f7] { color: #EA5771\n}\n.color-m[data-v-64b339f7]{ color:#1E88E5}\n@media (max-width: 320px) {\n.card-icon[data-v-64b339f7]{ display: none;\n}\n}\n", ""]);
+exports.push([module.i, "\nhr[data-v-64b339f7]{ width: 100%\n}\n.md-button[data-v-64b339f7], .md-button-clean[data-v-64b339f7]{\r\n    background-color: white;\n}\n.card-icon[data-v-64b339f7] {\r\n    background: #e3f2fd;\r\n    display: flex;\r\n    align-items: center;\r\n    padding: 0px 8px 0px 16px;\n}\n.margin-x[data-v-64b339f7]{\r\n    margin: 0 25px;\r\n    padding-top: 32px;\n}\n.size-img[data-v-64b339f7]{ \r\n    width: 100px; \r\n    height: 100px;\n}\n.color-w[data-v-64b339f7] { color: #EA5771\n}\n.color-m[data-v-64b339f7]{ color:#1E88E5}\n@media (max-width: 320px) {\n.card-icon[data-v-64b339f7]{ display: none;\n}\n}\r\n", ""]);
 
 // exports
 
@@ -129503,8 +129503,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/PNI/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/PNI/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\PNI\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\PNI\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
